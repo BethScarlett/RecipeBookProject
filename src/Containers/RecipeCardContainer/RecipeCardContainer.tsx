7eecc9ffe56ad0fc5c6aca93634df48ee485d7de
Bethany Burrows <jedcburrows@gmail.com>
2024-06-04 14:31:38 +0100

    Pulled in mock data from API and logged in console.

--- src/Containers/RecipeCardContainer/RecipeCardContainer.tsx
+++ src/Containers/RecipeCardContainer/RecipeCardContainer.tsx
@@ -1,11 +1,11 @@
-import RecipeCard from "../../Components/RecipeCard/RecipeCard"
+import RecipeCard from "../../Components/RecipeCard/RecipeCard";
 
 const CardContainer = () => {
   return (
     <div>
       <RecipeCard />
     </div>
-  )
-}
+  );
+};
 
-export default CardContainer
+export default CardContainer;
---
+++ src/Types/Recipe.ts
@@ -0,0 +1,11 @@
+type Recipe = {
+  id: number;
+  img: string;
+  name: string;
+  madeBy: string;
+  desc: string;
+  ingredients: string[];
+  steps: string[];
+  category: string;
+  suitableFor: string[];
+};
