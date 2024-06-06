4a6012869195241b3fcfd8c50f3c134dd254db58
Bethany Burrows <jedcburrows@gmail.com>
2024-06-06 15:02:08 +0100

    Added searchbox to filter recipes by name and/or category

--- src/Containers/RecipeCardContainer/RecipeCardContainer.tsx
+++ src/Containers/RecipeCardContainer/RecipeCardContainer.tsx
@@ -2,15 +2,54 @@ import "./RecipeCardContainer.scss";
 import RecipeCard from "../../Components/RecipeCard/RecipeCard";
 import Recipe from "../../Types/Recipe";
 import { Link } from "react-router-dom";
+import { ChangeEvent, useEffect, useState } from "react";
 
 type RecipeCardContainerProps = {
   recipes: Recipe[];
 };
 
 const RecipeCardContainer = ({ recipes }: RecipeCardContainerProps) => {
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [filteredRecipes, setFilteredRecipes] = useState<Recipe[]>([]);
+
+  useEffect(() => {}, []);
+
+  useEffect(() => {
+    handleFilterRecipes(searchTerm);
+  }, [searchTerm]);
+
+  //TODO - Remove category filter when buttons are complete
+  const handleFilterRecipes = (searchTerm: string) => {
+    setFilteredRecipes(
+      recipes.filter((recipe) => {
+        if (
+          recipe.name.includes(searchTerm) ||
+          recipe.category.includes(searchTerm)
+        ) {
+          return recipe;
+        }
+      })
+    );
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const cleanInput =
+      e.currentTarget.value.slice(0, 1).toUpperCase() +
+      e.currentTarget.value.slice(1);
+    console.log(cleanInput);
+    setSearchTerm(cleanInput);
+  };
+
   return (
     <div className="card-container">
-      {recipes.map((recipe) => (
+      <label htmlFor="search">Search By Name/Category: </label>
+      <input
+        type="text"
+        className="card-container__search"
+        id="search"
+        onChange={handleInputChange}
+      />
+      {filteredRecipes.map((recipe) => (
         <Link key={recipe.id} to={`/recipe/${recipe.id}`}>
           <RecipeCard name={recipe.name} description={recipe.description} />
         </Link>
