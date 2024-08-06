98d0802f1a5d06bef9cb4cbd72d9c5eb9cb14873
Bethany Burrows <jedcburrows@gmail.com>
2024-08-06 09:50:47 +0100

    Added link to the create recipe page and adjusted endpoint. Additionally added burger menu for additional functionality.

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,17 +1,33 @@
-//import NavbarButtons from "../NavbarButtons/NavbarButtons"
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
+import NavbarButtons from "../NavbarButtons/NavbarButtons";
 import "./Navbar.scss";
+import { useState } from "react";
 
 type NavbarProps = {
   heading?: string;
 };
 
 const Navbar = ({ heading }: NavbarProps) => {
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
+  const handleToggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="navbar">
       <h1 className="navbar__heading">{heading}</h1>
       <div className="navbar__buttons">
-        {/* TODO - Add back later */}
-        {/* <NavbarButtons buttonOneHeading="View" buttonTwoHeading="Create" buttonThreeHeading="Update"/> */}
+        <div onClick={handleToggleMenu}>
+          <BurgerMenu />
+        </div>
+        {showMenu ? (
+          <NavbarButtons
+            buttonOneHeading="View"
+            buttonTwoHeading="Create"
+            buttonThreeHeading="Update"
+          />
+        ) : null}
       </div>
     </div>
   );
--- src/Components/NavbarButtons/NavbarButtons.tsx
+++ src/Components/NavbarButtons/NavbarButtons.tsx
@@ -1,20 +1,31 @@
 import Button from "../Button/Button";
-import "./NavbarButtons.scss"
+import "./NavbarButtons.scss";
+import { Link } from "react-router-dom";
 
 type NavbarButtonsProps = {
-    buttonOneHeading: string;
-    buttonTwoHeading: string;
-    buttonThreeHeading: string;
+  buttonOneHeading: string;
+  buttonTwoHeading: string;
+  buttonThreeHeading: string;
+};
+
+{
+  /* TODO - Rework to take in number and string array & use these to populate buttons ?*/
 }
 
-const NavbarButtons = ({buttonOneHeading, buttonTwoHeading, buttonThreeHeading} : NavbarButtonsProps) => {
+const NavbarButtons = ({
+  buttonOneHeading,
+  buttonTwoHeading,
+  buttonThreeHeading,
+}: NavbarButtonsProps) => {
   return (
     <div className="navbar-buttons">
-      <Button heading={buttonOneHeading}/>
-      <Button heading={buttonTwoHeading}/>
-      <Button heading={buttonThreeHeading}/>
+      <Button heading={buttonOneHeading} />
+      <Link to={"/create"}>
+        <Button heading={buttonTwoHeading} />
+      </Link>
+      <Button heading={buttonThreeHeading} />
     </div>
-  )
-}
+  );
+};
 
-export default NavbarButtons
+export default NavbarButtons;
