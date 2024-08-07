666ee298a8531196f6b0badd8eaafbaa3a428524
Bethany Burrows <jedcburrows@gmail.com>
2024-08-07 13:12:02 +0100

    Added login API call.

--- src/Pages/Login/LoginPage.tsx
+++ src/Pages/Login/LoginPage.tsx
@@ -1,10 +1,37 @@
+import { useNavigate } from "react-router-dom";
 import LoginForm from "../../Components/LoginForm/LoginForm";
+import Login from "../../Types/Login";
 import "./LoginPage.scss";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const handleLogin = async (details: Login) => {
+    try {
+      const response = await fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(details),
+      });
+      const result = await response.json();
+      if (result == true) {
+        navigate("/");
+      } else console.log("No user found");
+      console.log(result);
+    } catch (Error) {
+      console.log(Error);
+    }
+  };
+
   return (
     <div className="loginpage">
-      <LoginForm userNotFound={false} formType="login" />
+      <LoginForm
+        handleLogin={handleLogin}
+        userNotFound={false}
+        formType="login"
+      />
     </div>
   );
 };
